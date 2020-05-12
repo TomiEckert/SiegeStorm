9dec1181f7bba87eb25223b06c8f6106c3594c3b
TomiEckert <tamas.eckert@student.nhlstenden.com>
2020-05-12 18:39:37 +0200

    Added abstracts

---
+++ Abstracts/GameObject.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiegeStorm
+{
+    public abstract class GameObject
+    {
+        Vector2 position;
+        Texture2D texture;
+
+        protected Vector2 Position { get => position; }
+        protected Texture2D Texture { get => texture; }
+
+        protected void SetTexture(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+        protected void SetPosition(Vector2 position)
+        {
+            this.position = position;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(GameTime gameTime)
+        {
+            SiegeStorm.SpriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+}
---
+++ Abstracts/GameScreen.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiegeStorm
+{
+    public abstract class GameScreen
+    {
+        List<GameObject> gameObjects;
+
+        public GameScreen()
+        {
+            gameObjects = new List<GameObject>();
+        }
+
+        /// <summary>
+        /// Returns an array of GameObjects
+        /// </summary>
+        /// <returns>Array of GameObjects</returns>
+        protected GameObject[] GetObjects()
+        {
+            return gameObjects.ToArray();
+        }
+
+        /// <summary>
+        /// Adds an object to the screen.
+        /// </summary>
+        /// <param name="gameObject">GameObject to add</param>
+        protected void AddObject(GameObject gameObject)
+        {
+            if (!gameObjects.Contains(gameObject))
+                gameObjects.Add(gameObject);
+        }
+
+        /// <summary>
+        /// Removes an object from the screen.
+        /// </summary>
+        /// <param name="gameObject">GameObject to remove</param>
+        protected void RemoveObject(GameObject gameObject)
+        {
+            if (gameObjects.Contains(gameObject))
+                gameObjects.Remove(gameObject);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            foreach (var gameObject in gameObjects) {
+                gameObject.Update(gameTime); // TODO Implement update priority
+            }
+        }
+
+        public virtual void Draw(GameTime gameTime)
+        {
+            foreach (var gameObject in gameObjects) {
+                gameObject.Draw(gameTime); // TODO Implement draw priority/layers
+            }
+        }
+    }
+}
