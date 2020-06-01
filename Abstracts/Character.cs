5a1187cd901014c2b97519c608a1de62297de485
Ami <ami.chaib@student.nhlstenden.com>
2020-06-01 12:37:53 +0200

    Added Character class and Item class

---
+++ Abstracts/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiegeStorm.Abstracts
+{
+    abstract class Character: GameObject
+    {
+        string name;
+        int level;
+        int basePower;
+        List<Item> equiptment;
+
+        public Character(string name, int basePower)
+        {
+            this.name = name;
+            this.level = 1;
+            this.basePower = 1;
+            this.equiptment = new List<Item>();
+        }
+    }
+}
---
+++ Abstracts/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiegeStorm.Abstracts
+{
+    class Item: GameObject
+    {
+        string name;
+        string description { get; set; }
+        int power;
+        int weight;
+
+        public Item(string name, string description, int power, int weight) {
+            this.name = name;
+            this.description = description;
+            this.power = power;
+            this.weight = weight;
+        }
+
+        public string getName(){
+            return this.name;
+        }
+        public int getPower(){
+            return this.power;
+        }
+        public int getWeight()
+        {
+            return weight;
+        }
+    }
+}
