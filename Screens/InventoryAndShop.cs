9419de99ea742576ff834606fd2aacb43c82042d
Sytze <sytze.westerdijk@student.nhlstenden.com>
2020-06-02 16:07:53 +0200

    Added basic inventory functionality

--- Screens/InventoryAndShop.cs
+++ Screens/InventoryAndShop.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using SiegeStorm.Abstracts;
 
 namespace SiegeStorm.Screens
 {
@@ -13,6 +14,9 @@ namespace SiegeStorm.Screens
         GameObjects.SettingsMenu.ButtonReturn returnButton;
         GameObjects.SettingsMenu.ButtonFullscreen fullscreenButton;
         GameObjects.SettingsMenu.ButtonFPS fpsButton;
+
+        List<Item> inventoryItems = new List<Item>();
+        int maxCapacity = 20;
     
         internal override void LoadContent()
         {
@@ -23,5 +27,24 @@ namespace SiegeStorm.Screens
             fpsButton = new GameObjects.SettingsMenu.ButtonFPS();
             AddObject(fpsButton);
         }
+
+        public void AddItemToInventory(Item item)
+        {
+            if (inventoryItems.Count <= maxCapacity)
+            {
+                inventoryItems.Add(item);
+            }
+            else
+            {
+                //TODO make this not print in the console but in the game
+                Console.WriteLine("Inventory is full.");
+            }
+        }
+
+        public void RemoveItemFromInventory(Item item)
+        {
+            inventoryItems.Remove(item);
+        }
+
     }
 }
