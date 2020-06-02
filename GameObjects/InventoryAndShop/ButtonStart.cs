d3db4933f526adca114dec3f2890a91cec4a24c0
Hylke <h.westerdijk@gmail.com>
2020-06-02 15:52:19 +0200

    Created inventoryandshop folder and added default button

---
+++ GameObjects/InventoryAndShop/ButtonStart.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace SiegeStorm.GameObjects.MainMenu
+{
+    class ButtonStart : GameButton
+    {
+        public ButtonStart()
+        {
+            SetTexture(SiegeStorm.TextureManager.GetTexture("playButton"));
+            var x = SiegeStorm.ScreenWidth / 2 - Texture.Width / 2;
+            var y = SiegeStorm.ScreenHeight / 10 * 5 - Texture.Height;
+            SetPosition(new Vector2(x, y));
+        }
+        public override void Pressed()
+        {
+            SiegeStorm.ScreenManager.ChangeScreenTo("Map");
+        }
+    }
+}
