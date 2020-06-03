9a50c45f1ab965fb349855334d880ed87b76fd5a
Sytze <sytze.westerdijk@student.nhlstenden.com>
2020-06-03 16:54:15 +0200

    Updated lane

--- GameObjects/Levels/Lane.cs
+++ GameObjects/Levels/Lane.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SiegeStorm.GameObjects.Characters.Enemies;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,13 @@ namespace SiegeStorm.GameObjects.Levels
     class Lane
     {
         int position;
+        int maxAmountEnemies;
+        List<Enemy> enemies;
+
         public Lane(int position)
         {
             this.position = position;
+            enemies = new List<Enemy>();
         }
 
         public int getPosition()
