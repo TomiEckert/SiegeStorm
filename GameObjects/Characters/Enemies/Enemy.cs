291754f6aa9fd0717601c14cadee6037d4ecfbd5
Hylke <h.westerdijk@gmail.com>
2020-06-15 14:25:02 +0200

    default animation for enemy

--- GameObjects/Characters/Enemies/Enemy.cs
+++ GameObjects/Characters/Enemies/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using SiegeStorm.Abstracts;
 
 namespace SiegeStorm.GameObjects.Characters.Enemies
@@ -6,14 +7,20 @@ namespace SiegeStorm.GameObjects.Characters.Enemies
     public class Enemy : Character
     {
         private int currentLane;
+        private Vector2 position;
+        private Animation walk;
+   
         public Enemy() : base("Enemy Lvl 1")
         {
             //TODO set texture
-            SetTexture(SiegeStorm.TextureManager.GetTexture(this));
+           // SetTexture(SiegeStorm.TextureManager.GetTexture(this));
+            
             //TODO set position
             var x = SiegeStorm.ScreenWidth - Texture.Width;
-            var y = SiegeStorm.ScreenHeight / 3 - Texture.Height;
-            Vector2 position = new Vector2(x, y);
+            var y = SiegeStorm.ScreenHeight / 3 - Texture.Height ;
+
+            position = new Vector2(x, y);
+            walk = SiegeStorm.AnimationManager.GetAnimation("default");
             // SetPosition(new Vector2(x, y));
         }
    
@@ -33,11 +40,15 @@ namespace SiegeStorm.GameObjects.Characters.Enemies
         }
         public override void Update(GameTime gameTime)
         {
-            if (Position.X < (SiegeStorm.ScreenWidth - Texture.Width))
-            {
-                SetPosition(new Vector2(Position.X + 5, Position.Y));
-            }
+            //if (Position.X < (SiegeStorm.ScreenWidth - Texture.Width))
+            //{
+            //    SetPosition(new Vector2(Position.X + 5, Position.Y));
+            //}
 
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            walk.Draw(gameTime, Position);
         }
         //TODO movement
         //TODO attack
