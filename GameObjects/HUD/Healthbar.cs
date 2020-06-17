ad62c2de030ee468cf12f6d10d7060d2265445ea
Sytze <sytze.westerdijk@student.nhlstenden.com>
2020-06-17 14:37:26 +0200

    Added basis for hud

---
+++ GameObjects/HUD/Healthbar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiegeStorm.GameObjects.HUD
+{
+    class Healthbar
+    {
+        public int maxHealth;
+        public int currentHealth;
+
+        public Healthbar()
+        {
+
+        }
+
+        public void Update(int currentHealth)
+        {
+            this.currentHealth = currentHealth;
+        }
+
+        public void Draw()
+        {
+            float healthPercentage = currentHealth / maxHealth;
+
+        }
+    }
+}
